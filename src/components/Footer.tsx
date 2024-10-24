32e2c02429ef432f6bdd3f451f381dcfe5be8781
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-10-24 20:31:35 +0530

    upated author name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
-
 export function Footer() {
   return (
     <footer className="bg-background border-t border-border">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <div className="text-center text-muted-foreground">
           <p>
-            Made with ❤️ by <a href="https://zed.impic.tech" className="hover:text-primary transition-colors">zaid</a> at <a href="https://community.impic.tech" className="hover:text-primary transition-colors">impic</a>
+            Made with ❤️ by <a href="https://zed.impic.tech" className="text-blue-500 hover:text-primary transition-colors">Zed</a> at <a href="https://community.impic.tech" className="text-blue-500 hover:text-primary transition-colors">impic</a>
           </p>
         </div>
       </div>
